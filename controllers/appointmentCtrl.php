deabf1b27d3fd42fb3dda02e0dc77b5cf4f921a7
NawelleBedr <bedrouni.nawelle@gmail.com>
2023-03-06 14:42:51 +0100

    Détails RDV fini 2

--- controllers/appointmentCtrl.php
+++ controllers/appointmentCtrl.php
@@ -7,8 +7,6 @@ require_once(__DIR__ . '/../models/Patient.php');
 $idAppointment = abs(intval(filter_input(INPUT_GET, 'idAppointment')));
 $appointment = Appointment::getAppointment($idAppointment);
 
-var_dump($appointment);
-die;
 /***************************************************************************************/
 /* ************* AFFICHAGE DES VUES **************************/
 
